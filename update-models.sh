5508512c2b344c375133e881af2cfe2351699dfb
Hleb Albau <hleb.albau@gmail.com>
2021-02-02 16:15:28 +0300

    Fix generator version

--- update-models.sh
+++ update-models.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 docker run --user "$(id -u):$(id -g)" --rm -v "${PWD}:/client" -v "${PWD}/../FaceRecognition-web-openapi:/definitions" \
-openapitools/openapi-generator-cli generate -g typescript-axios \
+openapitools/openapi-generator-cli:v5.0.0-beta2 generate -g typescript-axios \
 -i /definitions/index.yml -o /client/src -c /client/ts-generator-config.json \
 -t /client/generator-templates || exit 1
 

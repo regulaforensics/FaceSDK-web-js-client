c3e1a2683f7cc8948d40fbcb6f2bd0344c68f3ef
Danila Kislitsyn <dangost16@gmail.com>
2022-04-21 18:59:56 +0300

    add quality-config.json

--- example/cjs/index.js
+++ example/cjs/index.js
@@ -5,7 +5,7 @@ const {
 const fs = require("fs");
 
 (async () => {
-    let apiBasePath = process.env.API_BASE_PATH || "https://faceapi.regulaforensics.com"
+    let apiBasePath = process.env.API_BASE_PATH || "https://test-faceapi.regulaforensics.com"
 
     const face1 = fs.readFileSync('../face1.jpg').buffer
     const face2 = fs.readFileSync('../face2.jpg').buffer
@@ -29,10 +29,13 @@ const fs = require("fs");
     }
     console.log("-----------------------------------------------------------------")
 
-    const detectResponse = await sdk.matchingApi.detect({
+    const config = JSON.parse(fs.readFileSync('../quality-config.json', 'utf-8').toString())
+
+    const detectRequest = {
         image: face1, onlyCentralFace: false,
-        thumbnails: true, attributes: true
-    })
+        thumbnails: true, quality: {align: {type: 1, pad: [128, 128, 128]}, background: [128, 128, 128], config: config}
+    }
+    const detectResponse = await sdk.matchingApi.detect(detectRequest)
     const detectResults = detectResponse.results
 
     console.log("                         Detect Results                          ")

d2907077dde407a350fca3c41beb7a8c1bb32fb4
Danila Kislitsyn <dangost16@gmail.com>
2022-10-07 15:37:51 +0300

    add xrequest to ext class params

--- src/ext/person-api.ts
+++ src/ext/person-api.ts
@@ -6,46 +6,46 @@ import {ImageFields, PersonFields} from "../models";
 import {AxiosRequestConfig} from "axios";
 
 export class PersonApi extends GenPersonApi {
-    createPerson(personFields: PersonFields, options?: AxiosRequestConfig): any {
-        return super.createPerson(personFields, options).then(r => r.data);
+    createPerson(personFields: PersonFields, xRequestID?: string, options?: AxiosRequestConfig): any {
+        return super.createPerson(personFields, xRequestID, options).then(r => r.data);
     }
 
-    addImageToPerson(personId: number, imageFields: ImageFields, options?: AxiosRequestConfig): any {
+    addImageToPerson(personId: number, imageFields: ImageFields, xRequestID?: string, options?: AxiosRequestConfig): any {
         if (imageFields.image.content && typeof imageFields.image.content !== "string") {
             imageFields.image.content = converter.encode(imageFields.image.content)
         }
-        return super.addImageToPerson(personId, imageFields, options).then(r => r.data);
+        return super.addImageToPerson(personId, imageFields, xRequestID, options).then(r => r.data);
     }
 
-    deleteImageOfPerson(imageId: number, personId: number, options?: AxiosRequestConfig): any {
-        return super.deleteImageOfPerson(imageId, personId, options).then(r => r.data);
+    deleteImageOfPerson(imageId: number, personId: number, xRequestID?: string, options?: AxiosRequestConfig): any {
+        return super.deleteImageOfPerson(imageId, personId, xRequestID, options).then(r => r.data);
     }
 
-    deletePerson(personId: number, options?: AxiosRequestConfig): any {
-        return super.deletePerson(personId, options).then(r => r.data);
+    deletePerson(personId: number, xRequestID?: string, options?: AxiosRequestConfig): any {
+        return super.deletePerson(personId, xRequestID, options).then(r => r.data);
     }
 
-    getAllGroupsByPersonId(page: number, size: number, personId: number, options?: AxiosRequestConfig): any {
-        return super.getAllGroupsByPersonId(page, size, personId, options).then(r => r.data);
+    getAllGroupsByPersonId(page: number, size: number, personId: number, xRequestID?: string, options?: AxiosRequestConfig): any {
+        return super.getAllGroupsByPersonId(page, size, personId, xRequestID, options).then(r => r.data);
     }
 
-    getAllImagesByPersonId(page: number, size: number, personId: number, options?: AxiosRequestConfig): any {
-        return super.getAllImagesByPersonId(page, size, personId, options).then(r => r.data);
+    getAllImagesByPersonId(page: number, size: number, personId: number, xRequestID?: string, options?: AxiosRequestConfig): any {
+        return super.getAllImagesByPersonId(page, size, personId, xRequestID, options).then(r => r.data);
     }
 
-    getAllPersons(page: number, size: number, options?: AxiosRequestConfig): any {
-        return super.getAllPersons(page, size, options).then(r => r.data);
+    getAllPersons(page: number, size: number, xRequestID?: string, options?: AxiosRequestConfig): any {
+        return super.getAllPersons(page, size, xRequestID, options).then(r => r.data);
     }
 
-    getImageOfPerson(imageId: number, personId: number, options?: AxiosRequestConfig): any {
-        return super.getImageOfPerson(imageId, personId, options).then(r => r.data);
+    getImageOfPerson(imageId: number, personId: number, xRequestID?: string, options?: AxiosRequestConfig): any {
+        return super.getImageOfPerson(imageId, personId, xRequestID, options).then(r => r.data);
     }
 
-    getPerson(personId: number, options?: AxiosRequestConfig): any {
-        return super.getPerson(personId, options).then(r => r.data);
+    getPerson(personId: number, xRequestID?: string, options?: AxiosRequestConfig): any {
+        return super.getPerson(personId, xRequestID, options).then(r => r.data);
     }
 
-    updatePerson(personId: number, personFields: PersonFields, options?: AxiosRequestConfig): any {
-        return super.updatePerson(personId, personFields, options).then(r => r.data);
+    updatePerson(personId: number, personFields: PersonFields, xRequestID?: string, options?: AxiosRequestConfig): any {
+        return super.updatePerson(personId, personFields, xRequestID, options).then(r => r.data);
     }
 }
--- src/ext/search-api.ts
+++ src/ext/search-api.ts
@@ -5,10 +5,10 @@ import {AxiosRequestConfig} from "axios";
 
 
 export class SearchApi extends GenSearchApi {
-    search(searchRequest: SearchRequest, options?: AxiosRequestConfig): any {
+    search(searchRequest: SearchRequest, xRequestID?: string, options?: AxiosRequestConfig): any {
         if (searchRequest.image.content && typeof searchRequest.image.content !== "string") {
             searchRequest.image.content = converter.encode(searchRequest.image.content)
         }
-        return super.search(searchRequest, options).then(r => r.data);
+        return super.search(searchRequest, xRequestID, options).then(r => r.data);
     }
 }

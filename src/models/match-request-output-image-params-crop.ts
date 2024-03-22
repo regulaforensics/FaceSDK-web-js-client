7ab4a658eb7bf22e455c9d06ceda28105eebc7cd
GitHub Action <action@github.com>
2024-03-22 14:07:00 +0000

    Restructure docs test

--- src/models/match-request-output-image-params-crop.ts
+++ src/models/match-request-output-image-params-crop.ts
@@ -1,8 +1,8 @@
 /* tslint:disable */
 /* eslint-disable */
 /**
- * Regula FaceSDK Web API
- * [Download OpenAPI specification](https://github.com/regulaforensics/FaceSDK-web-openapi) ### Clients * [JavaScript](https://github.com/regulaforensics/FaceSDK-web-js-client) client for the browser and node.js based on axios * [Java](https://github.com/regulaforensics/FaceSDK-web-java-client) client compatible with jvm and android * [Python](https://github.com/regulaforensics/FaceSDK-web-python-client) 3.5+ client * [C#](https://github.com/regulaforensics/FaceSDK-web-csharp-client) client for .NET & .NET Core 
+ * Regula Face SDK Web API
+ * <a href=\"https://regulaforensics.com/products/face-recognition-sdk/  \" target=\"_blank\">Regula Face SDK</a> is a cross-platform biometric verification solution for a digital identity verification process. The SDK enables convenient and reliable face capture on the client side (mobile, web, and desktop) and further processing on the client or server side.  The Face SDK includes the following features:  * <a href=\"https://docs.regulaforensics.com/develop/face-sdk/overview/introduction/#face-detection\" target=\"_blank\">Face Detection</a> * <a href=\"https://docs.regulaforensics.com/develop/face-sdk/overview/introduction/#face-comparison-11\" target=\"_blank\">Face Match (1:1)</a> * <a href=\"https://docs.regulaforensics.com/develop/face-sdk/overview/introduction/#face-identification-1n\" target=\"_blank\">Face Search (1:N)</a> * <a href=\"https://docs.regulaforensics.com/develop/face-sdk/overview/introduction/#liveness-assessment\" target=\"_blank\">Liveness Assessment</a>  Here is the <a href=\"https://github.com/regulaforensics/FaceSDK-web-openapi  \" target=\"_blank\">OpenAPI specification on GitHub</a>.   ### Clients * [JavaScript](https://github.com/regulaforensics/FaceSDK-web-js-client) client for the browser and node.js based on axios * [Java](https://github.com/regulaforensics/FaceSDK-web-java-client) client compatible with jvm and android * [Python](https://github.com/regulaforensics/FaceSDK-web-python-client) 3.5+ client * [C#](https://github.com/regulaforensics/FaceSDK-web-csharp-client) client for .NET & .NET Core 
  *
  * The version of the OpenAPI document: 6.1.0
  * 
@@ -15,7 +15,7 @@
 
 
 /**
- * Whether to return the Base64 of an aligned and cropped portrait in the `crop` field.
+ * If set, a Base64-encoded aligned and cropped by the indicated settings portrait is returned in the `crop` field.
  * @export
  * @interface MatchRequestOutputImageParamsCrop
  */
@@ -27,7 +27,7 @@ export interface MatchRequestOutputImageParamsCrop {
      */
     'padColor'?: Array<number>;
     /**
-     * Whether to return the coordinates of the rectangle with the face in the original image prepared for the face crop.
+     * Whether to return the coordinates of the rectangle with the face in the original image prepared for the face crop. Can be used only if the face in the original image is not tilted.
      * @type {boolean}
      * @memberof MatchRequestOutputImageParamsCrop
      */
@@ -39,7 +39,7 @@ export interface MatchRequestOutputImageParamsCrop {
      */
     'size'?: Array<number>;
     /**
-     * The aspect ratio according to which face alignment is performed during face detection.
+     * The aspect ratio according to which face alignment is performed. See the [FaceImageQualityAlignType enum](https://docs.regulaforensics.com/develop/face-sdk/web-service/development/enums/face-image-quality-align-type/). To get a thumbnail with dimensions 3x4, set `0`.
      * @type {number}
      * @memberof MatchRequestOutputImageParamsCrop
      */

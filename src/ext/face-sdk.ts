a3f87088b122b85cf413adc7be4ded1843cfbf02
IlchenkoSergey <siarhei.ilchanka@regula.us>
2024-11-05 13:27:43 +0300

    remove healthckeck from public faceSDK interface

--- src/ext/face-sdk.ts
+++ src/ext/face-sdk.ts
@@ -1,7 +1,6 @@
 import { BASE_PATH } from '../base';
 import { Configuration } from '../configuration';
 import globalAxios, { AxiosInstance } from 'axios';
-import { HealthcheckApi } from './healthcheck-api';
 import { MatchApi } from './match-api';
 import { GroupApi } from './group-api';
 import { PersonApi } from './person-api';
@@ -10,7 +9,6 @@ import { DiagnosticsApi } from './diagnostics-api';
 import { Liveness20Api } from './liveness20-api';
 
 export class FaceSdk {
-    healthcheckApi: HealthcheckApi;
     matchApi: MatchApi;
     groupApi: GroupApi;
     personApi: PersonApi;
@@ -23,7 +21,6 @@ export class FaceSdk {
         protected basePath: string = BASE_PATH,
         protected axios: AxiosInstance = globalAxios,
     ) {
-        this.healthcheckApi = new HealthcheckApi(configuration, basePath, axios);
         this.matchApi = new MatchApi(configuration, basePath, axios);
         this.groupApi = new GroupApi(configuration, basePath, axios);
         this.personApi = new PersonApi(configuration, basePath, axios);

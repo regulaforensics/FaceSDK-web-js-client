72def50bac93bc764da7f1a115df3952dd0c75cc
Artem Tsybulko <tsybulko.artem@gmail.com>
2025-06-24 14:50:28 +0300

    SP-24000 - parcel -> vite

--- examples/basic/angular/src/main.ts
+++ examples/basic/angular/src/main.ts
@@ -1,5 +1,5 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { AppComponent } from './app/app.component';
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-    .catch((err: any) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
